35238323cca38925ae31714fa952c876900bba75
Xiaodong Liu <xiaodongliu@Xiaodongs-MacBook-Pro.local>
2023-02-25 19:19:57 -0500

    update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { Container, Button, Image, Embed } from 'semantic-ui-react';
-import myImage from '../images/answer.png';
+import myImage from '../images/whale3.jpg';
 import whaleVideo from "../video/whaleVideo.mp4";
 import whaleSong from '../sound/whaleSong.wav';
 import SlideShow from './SlideShow';
@@ -17,12 +17,12 @@ const HomePage = () => {
 				<Container text>
 					<h1>Joyce!</h1>
 					<h4>Riddle</h4>
-					<Container text style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+					<Container text>
 						<p>A bundle of <span style={{ color: "red" }}>Blubber</span></p>
 						<p>a sight for <span style={{ color: "orange" }}>Landlubbers</span></p>
 						<p>I wave with a slap-happy <span style={{ color: "yellow" }}>Tail</span></p>
 						<p>A saltwater  <span style={{ color: "blue" }}>Fountain</span></p>
-						<p>just sprayin' and spoutin'</p>
+						<p>just sprayin' and <span style={{ color: "green" }}>spoutin' </span></p>
 						<p>look out! Thar I blow! I'm a ...</p>
 					</Container>
 					<Container>
@@ -34,18 +34,18 @@ const HomePage = () => {
 							<source src={whaleSong} type="audio/wav" />
 							Your browser does not support the audio element.
 						</audio>}
-					{showAnswer && <video controls src={whaleVideo} type="video/mp4" />}
-					{/* {showAnswer && <Image src={myImage} fluid />} */}
+					{/* {showAnswer && <video controls src={whaleVideo} type="video/mp4" />} */}
+					{showAnswer && <Image src={myImage} fluid />}
 
 				</Container>
 
 				{/* <Footer version={global.appVersion} /> */}
 			</div>
-			<div>
+			{/* <div>
 				<Container text2>
 					<SlideShow slideArray={imageArray} />
 				</Container>
-			</div>
+			</div> */}
 		</>
 	);
 };
